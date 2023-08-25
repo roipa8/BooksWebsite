5023db6b34c88e8994b58ce42d2a0ab589140f58
roi <roipa@post.bgu.ac.il>
2023-08-25 18:43:17 +0300

    First commit

--- src/server.js
+++ src/server.js
@@ -6,17 +6,13 @@ import path from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// CHANGE THIS TO THE BUILD DIRECTORY
 const buildPath = path.join(__dirname, "../build");
 
 const app = express();
 const port = 3000;
 
-// Use express static middleware to serve the React build directory
 app.use(express.static(buildPath));
 
-// Adjust this to serve the built index.html for any unknown routes.
-// This helps support client-side routing.
 app.get("*", (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
 });
